1d6f46e0a19daa031d237ec5a4e5f625bb574486
Thiago Rabelo <thiagorabelojf@gmail.com>
2020-11-11 09:33:52 -0300

    add github ci and Generators propfile

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
   it('should render the colors correctly', () => {
     const { container } = render(<Main />)
+
+    expect(screen.getByRole('heading', { name: /Titulo/i })).toBeInTheDocument()
 
     expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
